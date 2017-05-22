a7836de2a593e35bcd37d38f0dda268c54e11cad
Khriskh <chrisqs16@hotmail.com>
2017-05-21 20:30:21 -0500

    SERVER-NEW

--- premake4.lua
+++ premake4.lua
@@ -1,61 +1,27 @@
-solution "ygo"
-    location "build"
-    language "C++"
-    objdir "obj"
+include "lzma"
 
-    configurations { "Debug", "Release" }
+project "ygopro"
+    kind "ConsoleApp"
+
+    files { "gframe.cpp", "config.h",
+            "game.cpp", "game.h",
+            "deck_manager.cpp", "deck_manager.h",
+            "data_manager.cpp", "data_manager.h",
+            "replay.cpp", "replay.h",
+            "netserver.cpp", "netserver.h",
+            "single_duel.cpp", "single_duel.h",
+            "tag_duel.cpp", "tag_duel.h",
+            "**.cc", "**.c"}
+    includedirs { "../ocgcore" }
+    links { "ocgcore", "clzma", "sqlite3", "lua" , "event"}
 
     configuration "windows"
-        defines { "WIN32", "_WIN32" }
-
-    configuration "bsd"
-        defines { "LUA_USE_POSIX" }
-        includedirs { "/usr/local/include" }
-        libdirs { "/usr/local/lib" }
-
-    configuration "macosx"
-        defines { "LUA_USE_MACOSX" }
-        includedirs { "/usr/local/include/*" }
-        libdirs { "/usr/local/lib", "/usr/X11/lib" }
-        buildoptions { "-stdlib=libc++" }
-        links {"OpenGL.framework","Cocoa.framework","IOKit.framework"}
-
-    configuration "linux"
-        defines { "LUA_USE_LINUX" }
-
-    configuration "vs*"
-        flags "EnableSSE2"
-        buildoptions { "-wd4996" }
-        defines { "_CRT_SECURE_NO_WARNINGS" }
+        files "ygopro.rc"
+        includedirs { "../event/include", "../sqlite3" }
+        links { "ws2_32" }
 
     configuration "not vs*"
-        buildoptions { "-fno-strict-aliasing", "-Wno-multichar" }
-    configuration {"not vs*", "windows"}
-        buildoptions { "-static-libgcc" }
-
-    configuration "Debug"
-        flags "Symbols"
-        defines "_DEBUG"
-        targetdir "bin/debug"
-
-    configuration { "Release", "not vs*" }
-        flags "Symbols"
-        defines "NDEBUG"
-        buildoptions "-march=native"
-
-    configuration { "Debug", "vs*" }
-        defines { "_ITERATOR_DEBUG_LEVEL=0" }
-
-    configuration "Release"
-        --flags { "OptimizeSpeed" }
-        targetdir "bin/release"
-
-    include "ocgcore"
-    include "gframe"
-    if os.is("windows") then
-    include "event"
-    include "freetype"
-    include "irrlicht"
-    include "lua"
-    include "sqlite3"
-    end
+        buildoptions { "-std=gnu++0x", "-fno-rtti" }
+    configuration "not windows"
+        includedirs { "/usr/include/lua", "/usr/include/lua5.2", "/usr/include/lua/5.2" }
+        links { "event_pthreads", "dl", "pthread" }
