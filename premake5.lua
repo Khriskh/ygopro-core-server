fc559297ed64c2a3d56a327503ea094c1488e3e4
khriskh <khriskh@gmail.com>
2018-04-07 17:41:43 -0500

    fix1

--- premake5.lua
+++ premake5.lua
@@ -1,67 +1,67 @@
 solution "ygo"
-    location "build"
-    language "C++"
-    objdir "obj"
+	location "build"
+	language "C++"
+	objdir "obj"
 	startproject "ygopro"
 
-    configurations { "Debug", "Release" }
+	configurations { "Debug", "Release" }
 	defines { "LUA_COMPAT_5_2" }
 
-    configuration "windows"
-        defines { "WIN32", "_WIN32" }
+	configuration "windows"
+		defines { "WIN32", "_WIN32" }
 
-    configuration "bsd"
-        defines { "LUA_USE_POSIX" }
-        includedirs { "/usr/local/include" }
-        libdirs { "/usr/local/lib" }
+	configuration "bsd"
+		defines { "LUA_USE_POSIX" }
+		includedirs { "/usr/local/include" }
+		libdirs { "/usr/local/lib" }
 
-    configuration "macosx"
-        defines { "LUA_USE_MACOSX" }
-        includedirs { "/usr/local/include/*" }
-        libdirs { "/usr/local/lib", "/usr/X11/lib" }
-        buildoptions { "-stdlib=libc++" }
-        links {"OpenGL.framework","Cocoa.framework","IOKit.framework"}
+	configuration "macosx"
+		defines { "LUA_USE_MACOSX" }
+		includedirs { "/usr/local/include/*" }
+		libdirs { "/usr/local/lib", "/usr/X11/lib" }
+		buildoptions { "-stdlib=libc++" }
+		links {"OpenGL.framework","Cocoa.framework","IOKit.framework"}
 
-    configuration "linux"
-        defines { "LUA_USE_LINUX" }
+	configuration "linux"
+		defines { "LUA_USE_LINUX" }
 
-    configuration "Release"
-        flags { "OptimizeSpeed" }
-        targetdir "bin/release"
+	configuration "Release"
+		flags { "OptimizeSpeed" }
+		targetdir "bin/release"
 
-    configuration "Debug"
-        symbols "On"
-        defines "_DEBUG"
-        targetdir "bin/debug"
+	configuration "Debug"
+		symbols "On"
+		defines "_DEBUG"
+		targetdir "bin/debug"
 
-    configuration { "Release", "vs*" }
-        flags { "StaticRuntime", "LinkTimeOptimization" }
-        disablewarnings { "4244", "4267", "4838", "4577", "4819", "4018", "4996", "4477" }
+	configuration { "Release", "vs*" }
+		flags { "StaticRuntime", "LinkTimeOptimization" }
+		disablewarnings { "4244", "4267", "4838", "4577", "4819", "4018", "4996", "4477" }
 
-    configuration { "Release", "not vs*" }
-        symbols "On"
-        defines "NDEBUG"
-        buildoptions "-march=native"
+	configuration { "Release", "not vs*" }
+		symbols "On"
+		defines "NDEBUG"
+		buildoptions "-march=native"
 
-    configuration { "Debug", "vs*" }
-        defines { "_ITERATOR_DEBUG_LEVEL=0" }
+	configuration { "Debug", "vs*" }
+		defines { "_ITERATOR_DEBUG_LEVEL=0" }
 
-    configuration "vs*"
-        flags "EnableSSE2"
-        defines { "_CRT_SECURE_NO_WARNINGS" }
+	configuration "vs*"
+		flags "EnableSSE2"
+		defines { "_CRT_SECURE_NO_WARNINGS" }
 
-    configuration "not vs*"
-        buildoptions { "-fno-strict-aliasing", "-Wno-multichar" }
+	configuration "not vs*"
+		buildoptions { "-fno-strict-aliasing", "-Wno-multichar" }
 
-    configuration {"not vs*", "windows"}
-        buildoptions { "-static-libgcc" }
+	configuration {"not vs*", "windows"}
+		buildoptions { "-static-libgcc" }
 
-    startproject "ygopro"
+	startproject "ygopro"
 
-    include "ocgcore"
-    include "gframe"
-    if os.ishost("windows") then
-    include "event"
-    include "lua"
-    include "sqlite3"
-    end
+	include "ocgcore"
+	include "gframe"
+	if os.is("windows") then
+	include "event"
+	include "lua"
+	include "sqlite3"
+	end
